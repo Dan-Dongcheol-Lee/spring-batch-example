acee24d709c60337fc37586e1c2d714bcd211650
Dan Dongcheol Lee <leedc123@gmail.com>
2013-03-08 22:55:25 +1100

    Removed BatchSchedulerConfig.

--- src/test/java/spring/batch/example/schedule/ScheduleJobTest.java
+++ src/test/java/spring/batch/example/schedule/ScheduleJobTest.java
@@ -5,18 +5,10 @@ import org.junit.runner.RunWith;
 import org.quartz.Scheduler;
 import org.quartz.Trigger;
 import org.quartz.TriggerBuilder;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobExecution;
-import org.springframework.batch.core.JobParametersBuilder;
-import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
-import spring.batch.example.configuration.BatchApplicationConfig;
-import spring.batch.example.configuration.BatchItemConfig;
-import spring.batch.example.configuration.BatchSchedulerConfig;
-import spring.batch.example.filetofile.FileToFileJobConfig;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = { BatchSchedulerConfig.class },
